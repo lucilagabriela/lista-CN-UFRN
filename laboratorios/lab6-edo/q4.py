62b93d05eb3c334e735f46a7cb8f7fbcf33826d3
Lucila Costa <lucilagdac@gmail.com>
2023-11-28 12:38:06 -0300

    Create q4.py

---
+++ laboratorios/lab6-edo/q4.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+exp = input()
+x0 = float(input())
+y0 = float(input())
+h = float(input())
+n = int(input())
+g = lambda x,y: eval(exp)
+
+def rk4(g, x0, y0, h, n):
+
+
+saida = 
+
+print(f'{saida:.3f}')
